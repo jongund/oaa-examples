9ca857e7dc0894f747bdc6a8724492df4f679056
Suyang Chen <schen154@illinois.edu>
2016-07-06 10:05:33 -0500

    button-3 bug fixes

--- examples/button-new/js/button-3.js
+++ examples/button-new/js/button-3.js
@@ -1,7 +1,6 @@
 function init() {
   // Create variables for the various buttons
   var alert1Button =  document.getElementById('alert1');
-
   // Add event listeners to the various buttons
   alert1Button.addEventListener('click', alertButtonEventHandler);
   alert1Button.addEventListener('keydown', alertButtonEventHandler);
@@ -13,24 +12,22 @@ function init() {
  * @function alertButtonEventHandler
  *
  * @desc  Handles events for the alert button
- */
+ */ 
 
 function alertButtonEventHandler(event) {
   var type = event.type;
   var message = 'Hej, hello, ciao, こんにちは';
-  
   // Grab the keydown and click events
   if (type === 'keydown' || type === 'keypress') {
     // If either enter or space is pressed, execute the funtion
-    if (event.keyCode === 13 || event.keyCode === 32) {
+    if ((event.keyCode === 32 && type === 'keypress')|| (event.keyCode === 13 && type === 'keypress')) {
       alert(message);
-      
       event.preventDefault();
-      event.stopPropagation();
     }
   } else if (type === 'click') {
     alert(message);
   }
+  
 }
 
 window.addEventListener('load', init);
