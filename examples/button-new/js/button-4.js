9494e2d8891bc8f6f3a6fe8c2b2814257c5b4341
Jon Gunderson <jongund@illinois.edu>
2016-07-06 09:21:48 -0500

    update button 4 to be anti pattern

--- examples/button-new/js/button-4.js
+++ examples/button-new/js/button-4.js
@@ -4,7 +4,6 @@ function init() {
 
   // Add event listeners to the various buttons
   alert2Button.addEventListener('click', alertButtonEventHandler);
-  alert2Button.addEventListener('keydown', alertButtonEventHandler);
 
 }
 
@@ -15,20 +14,10 @@ function init() {
  */
 
 function alertButtonEventHandler(event) {
-  var type = event.type;
   var message = 'Hej, hello, ciao, こんにちは';
   
-  // Grab the keydown and click events
-  if (type === 'keydown') {
-    // If either enter or space is pressed, execute the funtion
-    if (event.keyCode === 13 || event.keyCode === 32) {
-      alert(message);
-      
-      event.preventDefault();
-    }
-  } else if (type === 'click') {
-    alert(message);
-  }
+  alert(message);
+  
 }
 
 window.addEventListener('load', init);
